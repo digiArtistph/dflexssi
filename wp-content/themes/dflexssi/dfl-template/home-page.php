1e25b0918b395ce3eda527c850ff1e68893a1ddc
Kenneth P. Vallejos <kenn_vall@yahoo.com>
2014-06-15 12:53:49 +0800

    adds blog-page template

--- wp-content/themes/dflexssi/dfl-template/home-page.php
+++ wp-content/themes/dflexssi/dfl-template/home-page.php
@@ -3,78 +3,7 @@
 	Template Name: Home Page
 	Description: Home page for the site
 */ ?>
-<!DOCTYPE html>
-<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
-<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
-<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
-<!--[if (gte IE 9)|!(IE)]><!-->
-<html lang="en"> <!--<![endif]-->
-<head>
-
-    <!-- Basic Page Needs
-  ================================================== -->
-	<meta charset="utf-8">
-	<title>D-Flex Strategic Solutions Inc.</title>
-	<meta name="description" content="Free Html5 Templates and Free Responsive Themes Designed by Kimmy | zerotheme.com">
-	<meta name="author" content="www.zerotheme.com">
-	
-    <!-- Mobile Specific Metas
-  ================================================== -->
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    
-    <!-- CSS
-  ================================================== -->
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/zerogrid.css">
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/style.css">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/responsive.css">
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/responsiveslides.css" />
-	
-	<!--[if lt IE 8]>
-       <div style=' clear: both; text-align:center; position: relative;'>
-         <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
-           <img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
-        </a>
-      </div>
-    <![endif]-->
-    <!--[if lt IE 9]>
-		<script src="<?php echo get_template_directory_uri() ?>/js/html5.js"></script>
-		<script src="<?php echo get_template_directory_uri() ?>/js/css3-mediaqueries.js"></script>
-	<![endif]-->
-	
-	<link href='<?php echo get_template_directory_uri() ?>/images/favicon.ico' rel='icon' type='image/x-icon'/>
-	
-	<script src="<?php echo get_template_directory_uri() ?>/js/jquery.min.js"></script>
-	<script src="<?php echo get_template_directory_uri() ?>/js/responsiveslides.js"></script>
-	<script>
-    $(function () {
-      $("#slider").responsiveSlides({
-        auto: true,
-        pager: false,
-        nav: true,
-        speed: 500,
-        maxwidth: 962,
-        namespace: "centered-btns"
-      });
-    });
-  </script>
-    
-</head>
-<body>
-<!--------------Header--------------->
-<header> 
-	<div id="logo"><a href=""><img src="<?php echo get_template_directory_uri() ?>/images/logo.png"/></a></div>
-	
-	<nav>
-		<ul>
-			<li><a href="index.html">Home</a></li>
-			<li><a href="blog.html">Blog</a></li>
-			<li><a href="gallery.html">Gallery</a></li>
-			<li><a href="single.html">About</a></li>
-			<li><a href="single.html">Contact</a></li>
-		</ul>
-	</nav>
-</header>
-
+<?php get_header(); ?>
 <div class="social">
 		<div class="shareicons">
 			<ul>
--- wp-content/themes/dflexssi/header.php
+++ wp-content/themes/dflexssi/header.php
@@ -1,24 +1,84 @@
 <!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
 <title><?php wp_title( ' | ', true, 'right' ); ?></title>
+<!-- Mobile Specific Metas
+================================================== -->
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
 <?php wp_head(); ?>
+
+
+	
+    <!-- CSS
+  ================================================== -->
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/zerogrid.css">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/style.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/responsive.css">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/responsiveslides.css" />
+	
+	<!--[if lt IE 8]>
+       <div style=' clear: both; text-align:center; position: relative;'>
+         <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+           <img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
+        </a>
+      </div>
+    <![endif]-->
+    <!--[if lt IE 9]>
+		<script src="<?php echo get_template_directory_uri() ?>/js/html5.js"></script>
+		<script src="<?php echo get_template_directory_uri() ?>/js/css3-mediaqueries.js"></script>
+	<![endif]-->
+	
+	<link href='<?php echo get_template_directory_uri() ?>/images/favicon.ico' rel='icon' type='image/x-icon'/>
+	
+	<script src="<?php echo get_template_directory_uri() ?>/js/jquery.min.js"></script>
+	<script src="<?php echo get_template_directory_uri() ?>/js/responsiveslides.js"></script>
+	<script>
+    $(function () {
+      $("#slider").responsiveSlides({
+        auto: true,
+        pager: false,
+        nav: true,
+        speed: 500,
+        maxwidth: 962,
+        namespace: "centered-btns"
+      });
+    });
+  </script>
+    
 </head>
 <body <?php body_class(); ?>>
-<div id="wrapper" class="hfeed">
-<header id="header" role="banner">
-<section id="branding">
-<div id="site-title"><?php if ( ! is_singular() ) { echo '<h1>'; } ?><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php esc_attr_e( get_bloginfo( 'name' ), 'dflexssi' ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a><?php if ( ! is_singular() ) { echo '</h1>'; } ?></div>
-<div id="site-description"><?php bloginfo( 'description' ); ?></div>
-</section>
-<nav id="menu" role="navigation">
-<div id="search">
-<?php get_search_form(); ?>
-</div>
-<?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
-</nav>
+<!------------Header--------------->
+<header> 
+	<div id="logo"><a href=""><img src="<?php echo get_template_directory_uri() ?>/images/logo.png"/></a></div>
+	<nav>
+	<?php
+	$defaults = array(
+		'theme_location'  => '',
+		'menu'            => '',
+		'container'       => false,
+		'container_class' => 'hello',
+		'container_id'    => '',
+		'menu_class'      => 'menu',
+		'menu_id'         => '',
+		'echo'            => true,
+		'before'          => '',
+		'after'           => '',
+		'link_before'     => '',
+		'link_after'      => '',
+		'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+		'depth'           => 0,
+		'walker'          => ''
+	);
+
+	wp_nav_menu( $defaults );
+
+	?>
+	</nav>
 </header>
-<div id="container">
